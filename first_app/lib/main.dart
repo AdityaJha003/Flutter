c01562dc555b94e913827eab022f99d5bf2cfacb
AdityaJha <jha.aditya1608@gmail.com>
2024-03-01 10:00:02 +0530

    initial commit

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,47 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.lightBlue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('HEHE'),
-        ),
-        body: Container(
-          child: Column(
-            children: [
-              const Text(
-                'LOGIN PAGE',
-                style: TextStyle(fontSize: 30),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.red),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('HEHE'),
+            ),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    const Text(
+                      'LOGIN PAGE!',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Image.network(
+                        'https://images.pexels.com/photos/2451622/pexels-photo-2451622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const TextField(
+                      // obscureText: true,
+                      decoration: InputDecoration(labelText: 'Enter your name'),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const TextField(
+                      obscureText: true,
+                      decoration:
+                          InputDecoration(labelText: 'Enter your password'),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButton(
+                        onPressed: () => {}, child: const Text('Submit'))
+                  ],
+                ),
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              Image.network(
-                  'https://images.pexels.com/photos/733857/pexels-photo-733857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-              const SizedBox(
-                height: 30,
-              ),
-              const TextField(
-                decoration: InputDecoration(labelText: 'Enter your name'),
-              ),
-              const TextField(
-                decoration: InputDecoration(labelText: 'Enter your password'),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            )));
   }
 }
